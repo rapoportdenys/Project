0b386124f0216a0ccbb384ba3fc7cc4dff56309f
rapoportdenys <88731068+rapoportdenys@users.noreply.github.com>
2021-09-18 11:32:11 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import 'bootstrap';
+import '../css/style.scss'
+import '../css/about.scss'
+import '../css/team-member.scss'
+import '../css/contacts.scss'
+import '../css/cases.scss'
+import '../css/publications.scss'
+import '../css/publication-item.scss'
+import '../css/nav-tabs.scss'
